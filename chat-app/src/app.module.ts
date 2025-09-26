df8d3f05b93b825692973093918a452d60665bca
malaver25 <malaverroshan2@gmail.com>
2025-09-26 17:40:50 -0500

    chat

--- chat-app/src/app.module.ts
+++ chat-app/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [UsersModule, DatabaseModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- chat-app/src/chat/chat.gateway.ts
+++ chat-app/src/chat/chat.gateway.ts
@@ -1,21 +1,48 @@
-import {
-  WebSocketGateway,
-  SubscribeMessage,
-  MessageBody,
-  WebSocketServer,
-} from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer } from '@nestjs/websockets';
+import { ChatService } from './chat.service';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { UpdateChatDto } from './dto/update-chat.dto';
+import { OnModuleInit } from '@nestjs/common';
+import { webSocket } from 'rxjs/webSocket';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
-export class ChatGateway {
+@WebSocketGateway()
+export class ChatGateway implements OnModuleInit {
+  constructor(private readonly chatService: ChatService) {}
+
   @WebSocketServer()
-  server: Server;
+  public server: Server;
 
-  // Cuando un cliente envía un mensaje con el evento "message"
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string): void {
-    console.log('Mensaje recibido:', message);
-    // retransmitir el mensaje a todos los clientes conectados
-    this.server.emit('message', message);
+  onModuleInit() { 
+    this.server.on('connection', (socket: Socket) => {
+      console.log("El cliente se ha conectado: ", socket.id)
+      socket.on('disconnect', () => {
+        console.log("El cliente se ha desconectado: ", socket.id);
+      })
+   });
+
+  /*@SubscribeMessage('createChat')
+  create(@MessageBody() createChatDto: CreateChatDto) {
+    return this.chatService.create(createChatDto);
+  }
+
+  @SubscribeMessage('findAllChat')
+  findAll() {
+    return this.chatService.findAll();
+  }
+
+  @SubscribeMessage('findOneChat')
+  findOne(@MessageBody() id: number) {
+    return this.chatService.findOne(id);
+  }
+
+  @SubscribeMessage('updateChat')
+  update(@MessageBody() updateChatDto: UpdateChatDto) {
+    return this.chatService.update(updateChatDto.id, updateChatDto);
+  }
+
+  @SubscribeMessage('removeChat')
+  remove(@MessageBody() id: number) {
+    return this.chatService.remove(id);*/
   }
 }
