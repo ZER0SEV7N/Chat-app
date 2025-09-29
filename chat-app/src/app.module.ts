236a929b5305d643b59f6f1f6f9ca2855b5505ca
hp Gamers <danielenriquesinger0@gmail.com>
2025-09-28 22:10:23 -0500

    Auth, no me gusta los commit

--- chat-app/src/app.module.ts
+++ chat-app/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { ChatModule } from './chat/chat.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, ChatModule],
+  imports: [UsersModule, DatabaseModule, ChatModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
